ba6a4674766554caa7b8496176a0f43412f4ce5b
wvwall <waltervelardo@gmail.com>
2021-04-13 17:27:07 +0200

    Completed 3-4-6

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,10 @@ var app = new Vue ({
     el: '#root',
     data: {
      search: "",
-     films: []   
+     films: [] ,
+     series: [] ,
+     genresFilm: [],
+     select : '',
     },
     methods: {
         searchFilm: function() {
@@ -12,6 +15,20 @@ var app = new Vue ({
             .then((response) => {
               this.films = response.data.results;
             });
+            axios.get(`https://api.themoviedb.org/3/search/tv?api_key=1fca7b48e709a528e543e95bb52c20bd&language=it-IT&query=${this.search}`)
+            .then((response) => {
+              this.series = response.data.results;
+            });
+        },
+        starsVote: function (vote) {
+          return Math.round(vote / 2)
         }
+    },
+    mounted() {
+      axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=1fca7b48e709a528e543e95bb52c20bd")
+      .then((response) => {
+        this.genresFilm = response.data.genres;
+        console.log(response);
+      });
     },
   });
