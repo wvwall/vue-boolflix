b3ed83b14054d5b9bbcf7177f19a84ec41d9a1a8
wvwall <waltervelardo@gmail.com>
2021-04-15 10:02:03 +0200

    Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,20 +11,28 @@ var app = new Vue ({
     },
     methods: {
         searchFilm: function() {
+
+          /* chiamata film */
             axios.get(`https://api.themoviedb.org/3/search/movie?api_key=1fca7b48e709a528e543e95bb52c20bd&language=it-IT&query=${this.search}`)
             .then((response) => {
               this.films = response.data.results;
             });
+
+            /* chiamata serie */
             axios.get(`https://api.themoviedb.org/3/search/tv?api_key=1fca7b48e709a528e543e95bb52c20bd&language=it-IT&query=${this.search}`)
             .then((response) => {
               this.series = response.data.results;
             });
         },
+
+        /* voto da 1 a 1 0 decimale in un numero intero da 1 a 5 */
         starsVote: function (vote) {
           return Math.round(vote / 2)
         }
     },
     mounted() {
+
+      /* chiamata generi */
       axios.get("https://api.themoviedb.org/3/genre/movie/list?api_key=1fca7b48e709a528e543e95bb52c20bd")
       .then((response) => {
         this.genresFilm = response.data.genres;
@@ -32,3 +40,4 @@ var app = new Vue ({
       });
     },
   });
+
